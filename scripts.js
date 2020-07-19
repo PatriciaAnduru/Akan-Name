1ea2f072bd247e92092a769b25571b04138bb3fb
Patricia Anduru <santa@northpole.com>
2020-07-19 20:54:41 +0300

    add description paragraph to index.html

--- scripts.js
+++ scripts.js
@@ -8,46 +8,31 @@ function akaned(){
                     //       0         1       2       3       4       5       6
 
     var day = parseInt(document.getElementById("day").value);
-    // alert(day);      //test
     var month = parseInt(document.getElementById("month").value);
-    // alert(month)     //test
     var year = document.getElementById("year").value;
-    // alert(year);     //test
     var gender = document.getElementById("gender").value;
-    // alert(gender);   //test
-
     var date = new Date(year +"/" + month + "/"+day);
     var actualBirthDay = date.getDay();
-    // alert(actualBirthDay);                          //show index of week/sun=0 to sat=6
-    var akanDay = dayOfWeek[actualBirthDay];        //value of index from dayOfWeek
-    // alert(akanDay);                          //test show days for various dates //correct on dates
-  var invalidMonth = ( month<1 || month>12 || ((document.getElementById("month").value)=="") );        
-            //above boolean: month is <1 or >12 or empty
-  var invalidDay = ( day<1 || day>31 || ((document.getElementById("day").value)=="") );                
-            //above boolean: day is <1 or >31 or empty  
-  var invalidYear = (  year<1 || ( year>( new Date().getFullYear() ) ) || ((document.getElementById("year").value)=="") );
-           //above boolean: year is <1 or >current year or empty
-  var invalidGender = ( (gender=="selectdefault") );
-           //above boolean: gender is 'selectdefault' i.e no chosen gender of female or male
-  var invalidAll = ((invalidDay==true)&&(invalidMonth==true)&&(invalidYear==true)&&(invalidGender==true));
-           //above boolean is true if all inputs(day,month,year and gender) are wrong
-  var validFemale = ((invalidDay==false)&&(invalidMonth==false)&&(invalidYear==false)&&(gender==="Female"));
-           //above boolean: day and month and year are all correct and gender='Female'  
-  var validMale = ((invalidDay==false)&&(invalidMonth==false)&&(invalidYear==false)&&(gender==="Male"));
-           //above boolean: day and month and year are all correct and gender='Male' 
-  var febDates = ( ((month===2)&&(day==30)) || ((month===2)&&(day==31)) );
-           //above boolean: check for feb 30 and feb 31 inputs (non-existing dates) 
-  var aprJune =  ( ((month===4)&&(day==31)) || ((month===6)&&(day==31)) );
-           //above boolean: check for apr 31 and June 31 inputs (non-existing dates)
-  var septOct =  ( ((month===9)&&(day==31)) || ((month===11)&&(day==31)) );
-           //above boolean: check for Sept 31 and Nov 31 (non-existing dates)
+    // alert(actualBirthDay);                          
+    var akanDay = dayOfWeek[actualBirthDay];     
+    var invalidMonth = ( month<1 || month>12 || ((document.getElementById("month").value)=="") );        
+    var invalidDay = ( day<1 || day>31 || ((document.getElementById("day").value)=="") );                 
+    var invalidYear = (  year<1 || ( year>( new Date().getFullYear() ) ) || ((document.getElementById("year").value)=="") );
+    var invalidGender = ( (gender=="selectdefault") );
+    var invalidAll = ((invalidDay==true)&&(invalidMonth==true)&&(invalidYear==true)&&(invalidGender==true));
+    var validFemale = ((invalidDay==false)&&(invalidMonth==false)&&(invalidYear==false)&&(gender==="Female")); 
+    var validMale = ((invalidDay==false)&&(invalidMonth==false)&&(invalidYear==false)&&(gender==="Male"));
+    var febDates = ( ((month===2)&&(day==30)) || ((month===2)&&(day==31)) );
+    var aprJune =  ( ((month===4)&&(day==31)) || ((month===6)&&(day==31)) );
+    var septOct =  ( ((month===9)&&(day==31)) || ((month===11)&&(day==31)) );
+
   
     if ( invalidAll==true ) {
-     document.getElementById('result').textContent = "*INVALID DETAILS!";
+     document.getElementById('result').textContent = "*ENTER DETAILS!";
      document.getElementById("result").style.color = "red";
      document.getElementById("resultintro").style.display = "none";
     }
-   else if( invalidDay ) {  
+   else if( invalidDay==true) {  
      document.getElementById('result').textContent = "*Please enter a Valid Day Number from 1 - 31";
      document.getElementById("result").style.color = "red";      
      document.getElementById("resultintro").style.display = "none";
@@ -68,7 +53,7 @@ function akaned(){
      document.getElementById("resultintro").style.display = "none";
      }
    else if ( (febDates==true) || (aprJune==true) ||(septOct==true) ) {
-     document.getElementById('result').textContent = "*Please Select an existing Date.";
+     document.getElementById('result').textContent = "*SELECT AN EXISTING DATE.";
      document.getElementById("result").style.color = "red";
      document.getElementById("resultintro").style.display = "none";
      }
